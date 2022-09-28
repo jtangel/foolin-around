f7e24d5b79a53ca99c5894f701e7e8f29203920b
Jack Tangel <jackmtangel@gmail.com>
2022-09-28 09:03:59 -0500

    just class notes... learning python!

---
+++ classnotes.py
@@ -0,0 +1,42 @@
+fun = 4
+
+notFun = 4.4
+
+sum = fun + notFun
+print(type(sum), sum)
+
+
+import random 
+rand_num = random.randint(2,1000000)
+print(rand_num)
+
+practice = float(4)
+
+print (practice)
+
+# rfhwifh;owriefheoirfh
+
+name = "Zen"
+print("My name is " + name)
+
+name = "Zen"
+print("My name is", name)
+
+
+print("Hello " + str(42))		# output: Hello 42
+
+f_name = "Jack"
+l_name = "Tangel"
+age = 24
+love = "climbing"
+
+# x = "Hi all, my name is {} {}. I am {}, and I love {}".format(f_name, l_name, age, love)
+x = (f"Hi all, my name is {f_name} {l_name}. I am {age}, and I love {love}.")
+print(x.upper())
+print(x.count("I"))
+
+
+
+
+
+
