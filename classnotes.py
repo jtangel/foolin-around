f1fd500b2f7bf430b05895efbf31fe09ec7976c7
Jack Tangel <jackmtangel@gmail.com>
2022-09-28 17:36:44 -0500

    more notes

--- classnotes.py
+++ classnotes.py
@@ -162,3 +162,22 @@ for integer in range(0, len(countries)):
 for country in countries:
     print("Country: ", country)
     # Challenge 4: print the country here
+
+
+
+# Challenge 1:
+#   Fill in the missing code for the full_name function.
+#   Be sure to add the 2 parameters (and name them appropriately)
+#   Return the full name to get the desired output!
+
+def full_name(fname, lname):
+    return fname + " " + lname 
+
+name1 = full_name("Eddie", "Aikau")
+print(name1) # should print Eddie Aikau
+
+# Challenge 2: 
+#   Call the function again using your own name and print the results!
+
+name2 = full_name ("Jack", "Tangel")
+print(name2)
